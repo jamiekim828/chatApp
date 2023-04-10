c368973e4dac956497233d2fd67341fc0bf7b3e0
jamiekim828 <jamiekim828@gmail.com>
2023-04-10 22:49:51 +0200

    chatengine working with one user

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
-  return <ChatEngine />;
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID='a825b74c-d1e2-4d68-b3b2-c406beb0f84c'
+      userName='Jamie'
+      userSecret='123123'
+    />
+  );
 }
 
 export default App;
