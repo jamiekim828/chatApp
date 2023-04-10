2a610d26cfd4ec09434e90241ccd6b7153e88e13
jamiekim828 <jamiekim828@gmail.com>
2023-04-11 00:47:21 +0200

    chat feed structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       projectID='a825b74c-d1e2-4d68-b3b2-c406beb0f84c'
       userName='Jamie'
       userSecret='123123'
+      renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/> }
     />
   );
 }
