a0e86dd4546bd0c662eb9e4a4a573f7f56ec96f3
jamiekim828 <jamiekim828@gmail.com>
2023-04-11 00:53:11 +0200

    my message render

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-export default function MyMessage() {
+export default function MyMessage({ message }) {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt='message-attachent'
+        className='message-image'
+        style={{ float: 'right' }}
+      />
+    );
+  }
   return (
-    <div>MyMessage</div>
-  )
+    <div
+      className='message'
+      style={{
+        float: 'right',
+        marginRight: '18px',
+        color: 'white',
+        backgroundColor: '#3B2A50',
+      }}
+    >
+        {message.text}
+    </div>
+  );
 }
