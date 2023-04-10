69e15d0ee9433166b70ba11eb461976558b19a87
jamiekim828 <jamiekim828@gmail.com>
2023-04-11 01:04:19 +0200

    render all users message

--- src/components/TheirMessage.js
+++ src/components/TheirMessage.js
@@ -1,7 +1,39 @@
 import React from 'react'
 
-export default function TheirMessage() {
+export default function TheirMessage({lastMessage, message}) {
+
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
+
+
   return (
-    <div>TheirMessage</div>
+    <div className='message-row'>{
+        isFirstMessageByUser && (
+            <div className='message-avatar' style={{backgroundImage: `url(${message?.sender?.avatar})`}}/>
+        )
+    }
+    {
+        message?.attachments?.length > 0 ?
+             (
+              <img
+                src={message.attachments[0].file}
+                alt='message-attachent'
+                className='message-image'
+                style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px'}}
+              />
+            )
+            : (<div
+                className='message'
+                style={{
+                  float: 'left',
+                  backgroundColor: '#CABCDC',
+                  marginLeft: isFirstMessageByUser ? '4px': '48px'
+                }}
+              >
+                  {message.text}
+              </div>)
+          
+    }
+    
+    </div>
   )
 }
